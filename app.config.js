00ca2433dab66d31a31a07ba02a174c063b18f2b
alexisef23 <lexisfri23@gmail.com>
2026-07-24 13:18:46 -0600

    Actualizacion de version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "Portal Inttec & Daravisa",
     slug: "portal-inttec",
-    version: "1.2.3",
+    version: "1.2.4",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "appmovil",
@@ -14,7 +14,7 @@ module.exports = {
       }
     },
     android: {
-      versionCode: 41,
+      versionCode: 42,
       adaptiveIcon: {
         backgroundColor: "#E6F4FE",
         foregroundImage: "./assets/images/android-icon-foreground.png",
